8fb5c5837937fab6636726a13e9a332b044afbfc
Lee-JeongMin <leejm456@gmail.com>
2020-09-09 09:06:49 +0900

    채권분석 리포트 전처리, 병합코드 변경

--- 2. Pre-processing and Feature Selection/report_preprocessing.py
+++ 2. Pre-processing and Feature Selection/report_preprocessing.py
@@ -3,9 +3,12 @@ from ekonlpy.tag import Mecab
 from ekonlpy.sentiment import MPCK
 
 pdf = pd.read_csv('pdf_2005_2017.csv', index_col=0)
+
+# 필요한 컬럼만 남기기
+pdf = pdf[['date', 'title', 'content']]
 # print(len(pdf))
 
-pdf['token'], pdf['ngram'] = '', ''
+pdf['ngram'] = ''
 
 for idx , sent in zip(pdf.index, pdf['content']):
     if (idx % 10) == 0:
@@ -13,12 +16,15 @@ for idx , sent in zip(pdf.index, pdf['content']):
   
     mpck = MPCK()
 
-    # mpck.tokenize를 사용해 논문에서 사용하는 5개의 품사만 추출해 토큰화 한다.
+    # mpck.tokenize를 사용해 논문에서 사용하는 5개의 품사만 추출해 토큰화 한다. => uni-gram
     tokens = mpck.tokenize(sent)
-    pdf.loc[idx, 'token'] = tokens
+    # pdf.loc[idx, 'token'] = tokens
 
     # mpck.ngramize를 사용해 bi-gram ~ 5-gram까지 ngram을 나눈다.
     ngrams = mpck.ngramize(tokens)
-    pdf.loc[idx, 'ngram'] = ngrams
+    # pdf.loc[idx, 'ngrams'] = ngrams
 
-pdf.to_csv('./report_preprocessing.csv')
+    # 'token'컬럼과 '2-5gram'을 합쳐서 '1~5'그램을 만든다.
+    pdf['ngram'][idx] = tokens+ngrams
+
+pdf.to_csv('./report_preprocessing.csv', index=False)
